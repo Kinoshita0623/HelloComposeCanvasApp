2fdf765a24e6972fdbed0e69f759c83ba2c96fe7
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-07-15 01:29:11 +0900

    ジェスチャーのコントロール範囲などを変更しました

--- app/src/main/java/jp/panta/myapplication/MainActivity.kt
+++ app/src/main/java/jp/panta/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package jp.panta.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
@@ -14,9 +15,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import jp.panta.myapplication.ui.theme.HelloComposeCanvasAppTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.draw.clip
@@ -24,6 +23,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import kotlin.math.roundToInt
@@ -33,9 +33,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-            PaintInputTest(25.dp) {
+            PaintInputTest(25.dp, Modifier.fillMaxSize()) {
 
-                Text("hogehoge")
             }
         }
     }
@@ -43,148 +42,168 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun PaintInputTest(pointSize: Dp, block: @Composable BoxScope.()->Unit) {
+fun PaintInputTest(pointSize: Dp, modifier: Modifier, block: @Composable BoxScope.()->Unit) {
 
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(modifier = modifier) {
 
-        var offsetX by remember {
-            mutableStateOf(0f)
-        }
-        var offsetY by remember {
-            mutableStateOf(0f)
-        }
+        BoxWithConstraints {
+            val heightPx = with(LocalDensity.current) {
+                maxHeight.value.dp.toPx()
+            }
+            val widthPx = with(LocalDensity.current) {
+                maxWidth.value.dp.toPx()
+            }
+            val pointPx = with(LocalDensity.current) {
+                pointSize.value.dp.toPx()
+            }
+
+            val padding = with(LocalDensity.current) {
+                16.dp.toPx()
+            }
+
+            var offsetX by remember {
+                mutableStateOf(padding)
+            }
+            var offsetY by remember {
+                mutableStateOf(padding)
+            }
 
 
-        var offsetX2 by remember {
-            mutableStateOf(100f)
-        }
+            var offsetX2 by remember {
+                mutableStateOf(widthPx - padding - pointPx)
+            }
 
-        var offsetY3 by remember {
-            mutableStateOf(200f)
-        }
-        TransformableSample()
+            var offsetY3 by remember {
+                mutableStateOf(heightPx - padding - pointPx)
+            }
+            TransformableSample()
 
 
-        block.invoke(this)
+            block.invoke(this)
 
-        Canvas(modifier = Modifier.matchParentSize()) {
+            Canvas(modifier = Modifier.matchParentSize()) {
 
-            drawLine(
-                color = Color.Blue,
-                start = Offset(offsetX + pointSize.value + 3, offsetY + pointSize.value + 3),
-                end = Offset(offsetX2 + pointSize.value + 3, offsetY + pointSize.value + 3),
-                strokeWidth = 6f
-            )
+                drawLine(
+                    color = Color.Blue,
+                    start = Offset(offsetX + pointSize.value + 3, offsetY + pointSize.value + 3),
+                    end = Offset(offsetX2 + pointSize.value + 3, offsetY + pointSize.value + 3),
+                    strokeWidth = 6f
+                )
 
-            drawLine(
-                color = Color.Blue,
-                start = Offset(offsetX + pointSize.value + 3, offsetY + pointSize.value + 3),
-                end = Offset(offsetX + pointSize.value + 3, offsetY3 + pointSize.value + 3),
-                strokeWidth = 6f
-            )
+                drawLine(
+                    color = Color.Blue,
+                    start = Offset(offsetX + pointSize.value + 3, offsetY + pointSize.value + 3),
+                    end = Offset(offsetX + pointSize.value + 3, offsetY3 + pointSize.value + 3),
+                    strokeWidth = 6f
+                )
 
-            drawLine(
-                color = Color.Blue,
-                start = Offset(offsetX2 + pointSize.value + 3, offsetY + pointSize.value + 3),
-                end = Offset(offsetX2 + pointSize.value + 3, offsetY3 + pointSize.value + 3),
-                strokeWidth = 6f
-            )
+                drawLine(
+                    color = Color.Blue,
+                    start = Offset(offsetX2 + pointSize.value + 3, offsetY + pointSize.value + 3),
+                    end = Offset(offsetX2 + pointSize.value + 3, offsetY3 + pointSize.value + 3),
+                    strokeWidth = 6f
+                )
 
-            drawLine(
-                color = Color.Blue,
-                start = Offset(offsetX + pointSize.value + 3, offsetY3 + pointSize.value + 3),
-                end = Offset(offsetX2 + pointSize.value + 3, offsetY3 + pointSize.value + 3),
-                strokeWidth = 6f
-            )
-        }
+                drawLine(
+                    color = Color.Blue,
+                    start = Offset(offsetX + pointSize.value + 3, offsetY3 + pointSize.value + 3),
+                    end = Offset(offsetX2 + pointSize.value + 3, offsetY3 + pointSize.value + 3),
+                    strokeWidth = 6f
+                )
+            }
 
-        CirclePoint(
-            Modifier
-                .offset {
-                    IntOffset(offsetX.roundToInt(), offsetY.roundToInt())
-                }
-                .size(pointSize)
-                .pointerInput(Unit) {
-                    detectDragGestures { change, dragAmount ->
-                        change.consumeAllChanges()
-                        if (offsetX + 100 + dragAmount.x < offsetX2) {
-                            offsetX += dragAmount.x
-                        }
-                        if (offsetY + dragAmount.y + 100 < offsetY3) {
-                            offsetY += dragAmount.y
-                        }
-
+            CirclePoint(
+                Modifier
+                    .offset {
+                        IntOffset(offsetX.roundToInt(), offsetY.roundToInt())
                     }
-                }
+                    .size(pointSize)
+                    .pointerInput(Unit) {
+                        detectDragGestures { change, dragAmount ->
+                            change.consumeAllChanges()
+                            if (offsetX + 100 + dragAmount.x < offsetX2) {
+                                offsetX += dragAmount.x
+                            }
+                            if (offsetY + dragAmount.y + 100 < offsetY3) {
+                                offsetY += dragAmount.y
+                            }
 
-        )
-
-        CirclePoint(
-            Modifier
-                .offset {
-                    IntOffset(offsetX2.roundToInt(), offsetY.roundToInt())
-                }
-                .size(pointSize)
-                .pointerInput(Unit) {
-                    detectDragGestures { change, dragAmount ->
-                        change.consumeAllChanges()
-
-                        if (offsetX2 + dragAmount.x > offsetX + 100) {
-                            offsetX2 += dragAmount.x
-
-                        }
-                        if (offsetY + dragAmount.y + 100 < offsetY3) {
-                            offsetY += dragAmount.y
-                        }
-
-                    }
-                }
-
-        )
-
-        CirclePoint(
-            Modifier
-                .offset {
-                    IntOffset(offsetX2.roundToInt(), offsetY3.roundToInt())
-                }
-                .size(pointSize)
-                .pointerInput(Unit) {
-                    detectDragGestures { change, dragAmount ->
-                        change.consumeAllChanges()
-                        if (offsetX2 + dragAmount.x > offsetX + 100) {
-                            offsetX2 += dragAmount.x
-                        }
-                        if (offsetY3 + dragAmount.y > offsetY + 100) {
-                            offsetY3 += dragAmount.y
                         }
                     }
-                }
 
-        )
+            )
 
-        CirclePoint(
-            Modifier
-                .offset {
-                    IntOffset(offsetX.roundToInt(), offsetY3.roundToInt())
-                }
-                .size(pointSize)
-                .pointerInput(Unit) {
-                    detectDragGestures { change, dragAmount ->
-                        change.consumeAllChanges()
-                        if (offsetX + dragAmount.x + 100 < offsetX2) {
-                            offsetX += dragAmount.x
-                        }
-                        if (offsetY3 + dragAmount.y > offsetY + 100) {
+            CirclePoint(
+                Modifier
+                    .offset {
+                        IntOffset(offsetX2.roundToInt(), offsetY.roundToInt())
+                    }
+                    .size(pointSize)
+                    .pointerInput(Unit) {
+                        detectDragGestures { change, dragAmount ->
+                            change.consumeAllChanges()
 
-                            offsetY3 += dragAmount.y
+                            if (offsetX2 + dragAmount.x > offsetX + 100) {
+                                offsetX2 += dragAmount.x
+
+                            }
+                            if (offsetY + dragAmount.y + 100 < offsetY3) {
+                                offsetY += dragAmount.y
+                            }
+
                         }
                     }
-                }
-        )
+
+            )
+
+            CirclePoint(
+                Modifier
+                    .offset {
+                        IntOffset(offsetX2.roundToInt(), offsetY3.roundToInt())
+                    }
+                    .size(pointSize)
+                    .pointerInput(Unit) {
+                        detectDragGestures { change, dragAmount ->
+                            change.consumeAllChanges()
+                            if (offsetX2 + dragAmount.x > offsetX + 100) {
+                                offsetX2 += dragAmount.x
+                            }
+                            if (offsetY3 + dragAmount.y > offsetY + 100) {
+                                offsetY3 += dragAmount.y
+                            }
+                        }
+                    }
+
+            )
+
+            CirclePoint(
+                Modifier
+                    .offset {
+                        IntOffset(offsetX.roundToInt(), offsetY3.roundToInt())
+                    }
+                    .size(pointSize)
+                    .pointerInput(Unit) {
+                        detectDragGestures { change, dragAmount ->
+                            change.consumeAllChanges()
+                            if (offsetX + dragAmount.x + 100 < offsetX2) {
+                                offsetX += dragAmount.x
+                            }
+                            if (offsetY3 + dragAmount.y > offsetY + 100) {
+
+                                offsetY3 += dragAmount.y
+                            }
+                        }
+                    }
+            )
 
 
 
+        }
     }
+
+
+
+
 }
 
 @Composable
@@ -207,28 +226,39 @@ fun TransformableSample() {
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
 
+    Text("zoom:$zoom}")
     Box(
         Modifier.fillMaxSize()
     ) {
         Box(
             Modifier
-                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
-                .graphicsLayer(
-                    scaleX = zoom,
-                    scaleY = zoom,
-                )
-                .background(Color.Green)
                 .pointerInput(Unit) {
                     detectTransformGestures(
                         onGesture = { _, pan, gestureZoom, _ ->
-                            zoom *= gestureZoom
+                            Log.d("zoom size", "zoom:$zoom")
+                            if(zoom * gestureZoom in 0.5..4.0) {
+                                zoom *= gestureZoom
+                            }
                             offsetX += (pan.x * zoom)
                             offsetY += (pan.y * zoom)
                         }
                     )
                 }
-                .size(100.dp)
-        )
+                .fillMaxSize()
+        ) {
+            Box(
+                Modifier
+                    .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                    .graphicsLayer {
+                        scaleX = zoom
+                        scaleY = zoom
+                    }
+                    .height(100.dp)
+                    .fillMaxWidth()
+                    .background(Color.Green)
+
+            )
+        }
     }
 
 }
